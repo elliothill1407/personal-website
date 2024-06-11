25a4da7a1d08e349e08c9050e2433c941ade3adf
Elliot Hill <elliothill1407@gmail.com>
2024-06-11 23:54:51 +1000

    update configs

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Elliot Hill.
+      <time>{YEAR}</time> © Elliot Leslie Hill.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
